03c690aa7fc309ef2b23abd074aa12d9103efabc
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-19 23:34:13 +0800

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-Hello.<br>
-<?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
-  echo "\nYour connection is NOT encrypted.";
-}
-else {
-  echo "\nYour connection is encrypted!";
-}
-
